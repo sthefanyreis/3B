84f6695a99c95b989b7ca0915c0aee090baf24c1
sthefany <110991751+sthefanyreis@users.noreply.github.com>
2024-03-21 08:07:45 -0300

    21/03
    21/03

---
+++ main.js
@@ -0,0 +1,18 @@
+const botoes=document.querySelectorAll(".botao");
+console.log(botoes)
+for (let i=0; i < botoes.length;i++) {
+    console.log(i);
+}
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick=function() {
+        botoes[i].classList.add("ativo");
+    }
+}
+
+for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
+
+for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove("ativo");
+}
+botoes[i].classList.add("ativo");
+}
+}
